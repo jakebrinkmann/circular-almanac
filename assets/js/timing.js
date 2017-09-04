73323b4b158d4e56c06c922a0d064e967f463c4d
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-09-04 10:46:59 -0500

    ENH: add astronomical seasons

---
+++ assets/js/timing.js
@@ -0,0 +1,21 @@
+var colors = {
+	spring: "#2ECC40",
+	summer: "#FF4136",
+	fall: "#FF851B",
+	winter: "#AAAAAA"
+}
+
+// Northern Hemisphere, Astronomical
+// TODO: Calculate these numerically
+var seasons = {
+    // Spring - March Equinox to June Solstice;
+    spring: "03/20/2018 16:15",
+    // Summer - June Solstice to September Equinox;
+    summer: "06/21/2018 10:07",
+    // Fall (autumn) - September Equinox to December Solstice
+    fall: "09/22/2017 20:02",
+    // Winter - December Solstice to March Equinox.
+    winter: "12/21/2017 16:28"
+}
+
+var displayTime = $.("#display#time");
